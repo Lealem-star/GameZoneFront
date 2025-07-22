fbac706172a03b38d332fed1f2cadd642f937f36
Lealem-star <123200012+Lealem-star@users.noreply.github.com>
2025-07-22 20:54:54 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_BASE_URL = 'http://localhost:5000/api'; 
-const API_BASE_URL = 'https://gursha-backend.vercel.app/api'
+const API_BASE_URL = 'https://gursha-backend-git-main-meseretlealem8-2171s-projects.vercel.app/api'
     
 // Create axios instance with base configuration
 const api = axios.create({
