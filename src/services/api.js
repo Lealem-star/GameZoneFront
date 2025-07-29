eae3748576467b715aaca87760f10ef2ac697a26
Lealem-star <123200012+Lealem-star@users.noreply.github.com>
2025-07-29 18:15:18 +0300

    Update api.js
    correct api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_BASE_URL = 'http://localhost:5000/api'; 
-const API_BASE_URL = 'https://gursha-backend.vercel.app/api'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
     
 // Create axios instance with base configuration
 const api = axios.create({
